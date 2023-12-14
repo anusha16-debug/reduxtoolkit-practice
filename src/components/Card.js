f631acc6b549db72329dffb4b2fa22ce46157bc7
Anusha Hublikar <63278025+anusha16-debug@users.noreply.github.com>
2023-12-14 11:50:26 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,20 @@
+import React from 'react'
 
+const Card = (props) => {
+   const {image, title, price} = props.cardProp
+   const handleAddToCart = () => {
+    // Call the onAddToCart function with the product ID
+    props.onAddToCart(props.cardProp);
+  };
+
+  return (
+    <div className='card'>
+        <img src={image} alt='product'/>
+        <h4>{title}</h4>
+        <h5>{price}</h5>
+        <button className='btn' onClick={handleAddToCart}>Add to cart</button>
+    </div>
+  )
+}
+
+export default Card
