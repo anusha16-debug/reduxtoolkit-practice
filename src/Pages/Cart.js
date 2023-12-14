feb2f453f456df6a12d861736637a1d8acc9a0ba
Anusha Hublikar <63278025+anusha16-debug@users.noreply.github.com>
2023-12-14 11:53:06 +0530

    Update Cart.js

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,1 +1,33 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { removeItem } from '../store/cartSlice';
 
+const Cart = () => {
+    const dispatch = useDispatch()
+    const cartItems = useSelector(state => state.cart)
+
+
+    const handleRemoveItem = (id) => {
+        dispatch(removeItem(id))
+    }
+    return (
+        <div>
+            <h3>Cart</h3>
+            <div className='cartWrapper'>
+                {
+                    cartItems.map((item) => (
+                        <div className='cartCard' key={item.id}>
+                            <img src={item.image} alt='product' />
+                            <h4>{item.title}</h4>
+                            <h5>{item.price}</h5>
+                            <button className='btn' onClick={() => handleRemoveItem(item.id)}>Remove</button>
+                        </div>
+                    ))
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default Cart
